07e930171063a91002110779a018c261f8173377
nffie <ff3sta@gmail.com>
2024-12-27 10:37:42 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="system-card-viewer-container">
+      <div className="system-card-container">
+        <div className="header-container">
+          <h3>h3</h3>
+          <h4>h4</h4>
+        </div>
+        <div className="h-container">
+          <div className="v-container">
+            <div className="h-container">
+              <a className="link" href="#">
+                link
+              </a>
+              <a className="link" href="#">
+                link
+              </a>
+            </div>
+            <div className="h-container">
+              <a className="link doc" href="#" title="title">
+                link doc
+              </a>
+              <a className="link doc" href="#" title="title">
+                link doc
+              </a>
+              <a className="link doc" href="#" title="title">
+                link doc
+              </a>
+            </div>
+            <div className="h-container">
+              <a className="link main" href="#" title="title">
+                link main
+              </a>
+              <a className="link" href="#" title="title">
+                link
+              </a>
+              <a className="link" href="#" title="title">
+                link
+              </a>
+              <a className="link" href="#" title="title">
+                link
+              </a>
+              <a className="link" href="#" title="title">
+                link
+              </a>
+            </div>
+            <div className="h-container">
+              <a className="link main" href="#" title="title">
+                link main
+              </a>
+            </div>
+            <div className="h-container">
+              <a className="link" href="#" title="title">
+                link
+              </a>
+              <a className="link" href="#" title="title">
+                link
+              </a>
+            </div>
+            <div className="h-container">empty</div>
+          </div>
+          <div className="v-container">empty</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
